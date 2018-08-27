369558c20cc9b642d2f1f5abf18a9a328905931b
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-08-26 22:30:51 -0500

    Add files via upload

---
+++ Hyperparameter_tuning/Higgs_100.py
@@ -0,0 +1,17 @@
+from keras.layers import Dense, Activation, Dropout
+
+K.clear_session()
+tb_call = keras.callbacks.TensorBoard(log_dir='C:\\Users\\tkalnik\\PycharmProjects\\RIIAA_Escuela18-master\\logs2', histogram_freq=0, write_graph=True, write_images=True)
+
+model = Sequential()
+model.add(Dense(units=100, input_dim=X_train.shape[-1], kernel_initializer='TruncatedNormal', activation='elu'))
+model.add(Dense(units=100, input_dim=X_train.shape[-1], kernel_initializer='TruncatedNormal', activation='elu'))
+model.add(Dense(units=100, input_dim=X_train.shape[-1], kernel_initializer='TruncatedNormal', activation='elu'))
+model.add(Dense(units=100, input_dim=X_train.shape[-1], kernel_initializer='TruncatedNormal', activation='elu'))
+model.add(Dense(units=100, input_dim=X_train.shape[-1], kernel_initializer='TruncatedNormal', activation='elu'))
+model.add(Dense(units=100, input_dim=X_train.shape[-1], kernel_initializer='TruncatedNormal', activation='elu'))
+model.add(Dropout(0.2))
+model.add(Dense(1, kernel_initializer='TruncatedNormal', activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer="Adam", metrics=['accuracy'])
+print(model.summary())
+model.fit(X_train, Y_train, epochs=200, batch_size=28, callbacks=[tb_call])
